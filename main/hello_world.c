ddf11ed4adf655d60b6575133a544f0b4fc169cc
solareenlo <solareenlo@gmail.com>
2019-03-01 19:11:20 +0900

    tab -> 4 spaces

--- main/hello_world.c
+++ main/hello_world.c
@@ -1,8 +1,8 @@
 #include <stdio.h>
 
 int main(void) {
-	printf("Hello World.\n");
-	printf("ABC\n");
-	printf("日本語でも大丈夫\n");
-	return 0;
+    printf("Hello World.\n");
+    printf("ABC\n");
+    printf("日本語でも大丈夫\n");
+    return 0;
 }
